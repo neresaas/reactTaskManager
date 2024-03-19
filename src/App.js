c41ed79593ba88c6bc0cb3efdfdd3ebc95793e40
Nerea <neresaas@gmail.com>
2024-03-19 16:50:45 +0100

    Create task with useRef

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
 
-  let [name, setName] = useState('')
-  let [place, setPlace] = useState('')
-  let [priority, setPriority] = useState(0)
+  let [name, setName] = useState('');
+  let [place, setPlace] = useState('');
+  let [priority, setPriority] = useState(0);
+
+  let nameRef = useRef('');
+  let placeRef = useRef('');
+  let priorityRef = useRef(0);
+
   let [task, setTask] = useState([
     {
       name: 'Task1',
@@ -28,13 +33,28 @@ function App() {
     setTask([...task, newTask])
   }
 
+  let addTaskWithRefs = () => {
+    let newTask = {
+      name: nameRef.current.value,
+      place: placeRef.current.value,
+      priority: priorityRef.current.value
+    }
+    setTask([...task, newTask])
+  }
+
   return (
     <div>
-      <h2>Add task</h2>
+      <h2>Add task State</h2>
       <input type='text' placeholder='name' onChange={(e) => {setName(e.currentTarget.value)}}></input>
       <input type='text' placeholder='place' onChange={(e) => {setPlace(e.currentTarget.value)}}></input>
       <input type='number' placeholder='priority' onChange={(e) => {setPriority(parseInt(e.currentTarget.value))}}></input>
       <button onClick={addTask}> Add task </button>
+
+      <h2> Add task Ref</h2>
+      <input ref={nameRef} type='text' placeholder='name'></input>
+      <input ref={placeRef} type='text' placeholder='place'></input>
+      <input ref={priorityRef} type='number' placeholder='priority'></input>
+      <button onClick={addTaskWithRefs}> Add task </button>
 
       <ul>
         { task.map( t =>
