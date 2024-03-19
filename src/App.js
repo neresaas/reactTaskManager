c6a21a8f382a5b0924f6112b19df8a534d798afe
Nerea <neresaas@gmail.com>
2024-03-19 16:40:31 +0100

    add task input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useState, useRef, useEffect } from 'react';
 
 function App() {
 
+  let [name, setName] = useState('')
+  let [place, setPlace] = useState('')
+  let [priority, setPriority] = useState(0)
   let [task, setTask] = useState([
     {
       name: 'Task1',
@@ -15,8 +18,24 @@ function App() {
       priority: 0
     }
   ])
+
+  let addTask = () => {
+    let newTask = {
+      name: name,
+      place: place,
+      priority: priority
+    }
+    setTask([...task, newTask])
+  }
+
   return (
     <div>
+      <h2>Add task</h2>
+      <input type='text' placeholder='name' onChange={(e) => {setName(e.currentTarget.value)}}></input>
+      <input type='text' placeholder='place' onChange={(e) => {setPlace(e.currentTarget.value)}}></input>
+      <input type='number' placeholder='priority' onChange={(e) => {setPriority(parseInt(e.currentTarget.value))}}></input>
+      <button onClick={addTask}> Add task </button>
+
       <ul>
         { task.map( t =>
         <li>
